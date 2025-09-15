a36d462fa6ec7f349efd943e9a4af4957bd3bda6
Manjeet151 <111356540+Manjeet151@users.noreply.github.com>
2025-09-15 19:03:29 +0530

    Delete app1.py

--- app1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pickle
-
-# Load your CSV (the working file)
-popular_df = pd.read_csv('popular.csv')  # Make sure this CSV works locally
-
-# Save as a proper pickle
-with open('popular.pkl', 'wb') as f:
-    pickle.dump(popular_df, f)
